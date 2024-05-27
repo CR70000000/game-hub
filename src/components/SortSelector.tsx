6a2cd8b2f6f598bfdfac458d2e9a5cb251f029e0
GarCia <garcia1004@163.com>
2024-05-27 23:54:32 +0800

    游戏排序功能

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,37 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { BsChevronBarDown } from 'react-icons/bs'
 
-function SortSelector() {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void
+  sortOrder: string
+}
+
+function SortSelector({ onSelectSortOrder, sortOrder }: Props) {
+  // value: 后端返回的排序字段
+  // label: 前端显示的排序字段
+  const sortOrders = [
+    { value: '', label: '相关' },
+    { value: '-added', label: '添加日期' },
+    { value: 'name', label: '名称' },
+    { value: '-released', label: '发售日期' },
+    { value: '-metacritic', label: '人气' },
+    { value: '-rating', label: '平均评分' },
+  ]
+  const currentSortOrder = sortOrders.find((order) => order.value === sortOrder)
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
-        选项
+        选项:{currentSortOrder?.label || '相关' }
       </MenuButton>
       <MenuList>
-        <MenuItem>1</MenuItem>
-        <MenuItem>2</MenuItem>
-        <MenuItem>3</MenuItem>
-        <MenuItem>4</MenuItem>
-        <MenuItem>5</MenuItem>
-        <MenuItem>6</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   )
